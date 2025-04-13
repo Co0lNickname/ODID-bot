22cd8cafdf8eefba1e578b76aa9961ece70f0f5f
DocLivsey <doclivsey@github.com>
2025-04-13 20:11:36 +0300

    feat: start handler

--- routers/throwing.py
+++ routers/throwing.py
@@ -2,6 +2,12 @@ import structlog
 from aiogram import Router, types
 from aiogram.filters import Command
 
+from constants import HELLO_TEXT
+
 logger = structlog.get_logger(__name__)
 router = Router()
 
+
+@router.message(Command('start'))
+async def start_handler(message: types.Message):
+    await message.answer(HELLO_TEXT)
