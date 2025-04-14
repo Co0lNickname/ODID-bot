3156fe543872339ce59c90bd121b04b9d52fe4c7
DocLivsey <doclivsey@github.com>
2025-04-15 00:40:37 +0300

    feat: added base filters

--- routers/throwing.py
+++ routers/throwing.py
@@ -1,12 +1,14 @@
 import structlog
-from aiogram import Router, types
+from aiogram import Router, types, F
 from aiogram.filters import Command
 
 from constants import HELLO_TEXT
+from filters.chat_type import ChatTypeFilter
 from keyboards.for_throwing import throw_keyboard_button
 
 logger = structlog.get_logger(__name__)
 router = Router()
+router.message.filter(ChatTypeFilter(chat_type=["group", "supergroup"]))
 
 
 @router.message(Command('start'))
@@ -14,6 +16,6 @@ async def start_handler(message: types.Message):
     await message.answer(HELLO_TEXT, reply_markup=throw_keyboard_button())
 
 
-@router.message
+@router.message(F.text.lower() == 'бросок')
 async def throw_handler(message: types.Message):
     await message.answer('wow')
