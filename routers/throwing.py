5a44480ca3a88f7ee218e19b23f4412a5cc33f96
DocLivsey <doclivsey@github.com>
2025-04-16 13:15:23 +0300

    feat: returning to answering variant

--- routers/throwing.py
+++ routers/throwing.py
@@ -3,8 +3,6 @@ import random
 import structlog
 from aiogram import Router, types, F
 from aiogram.filters import Command
-from aiogram.enums.parse_mode import ParseMode
-from aiogram.exceptions import TelegramBadRequest
 
 from constants import HELLO_TEXT, throw_actions
 from filters.chat_type import ChatTypeFilter
@@ -23,13 +21,4 @@ async def start_handler(message: types.Message):
 @router.message(F.text.lower() == 'бросок')
 async def throw_handler(message: types.Message):
     action = random.choice(throw_actions)
-    reply_text = (
-        f'📝 пользователь {message.from_user.username if message.from_user.username else message.from_user.first_name}\n'
-        f'🎲 сделал бросок\n🏁 результат: *{action}*'
-    )
-    try:
-        await message.delete()
-        await message.answer(reply_text, parse_mode=ParseMode.MARKDOWN)
-    except TelegramBadRequest as exception:
-        if "message can't be deleted" in exception.message:
-            await message.answer('Для полного функционирования выдайте мне права на редактирование')
+    await message.reply(action)
