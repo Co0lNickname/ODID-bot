9c0dd13cff886e36ad0cec0b6c4778cc6789e87e
DocLivsey <doclivsey@users.noreply.github.com>
2025-04-17 22:36:48 +0300

    feat: moved onto throwing routing all handlers from main bot file

--- bot/ODID_bot.py
+++ bot/ODID_bot.py
@@ -1,12 +1,8 @@
-import random
-
 import structlog
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
+from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from routers.throwing import router as throwing_router
-from keyboards.for_throwing import throw_keyboard_button, confirm_participation_button
 from settings import telegram_bot_token
 
 logger = structlog.get_logger(__name__)
@@ -15,72 +11,6 @@ odid_bot = Bot(token=telegram_bot_token)
 storage = MemoryStorage()
 dispatcher = Dispatcher(storage=storage)
 
-# Используем словарь для хранения данных о пользователях
-group_members_map = {}
-# Словарь для отслеживания участников, подтвердивших участие
-confirmed_participants = {}
-
-@dispatcher.message(Command('start'))
-async def start_handler(message: types.Message):
-    chat_id = message.chat.id
-    if chat_id not in confirmed_participants:
-        confirmed_participants[chat_id] = set()
-    
-    await message.answer(
-        "👋 Привет! Подтверди участие, нажав на кнопку ниже.\n\nЕсли хотите завершить расчет, нажмите /finish",
-        reply_markup=confirm_participation_button()
-    )
-
-@dispatcher.message(lambda message: message.text == "Подтвердить участие")
-async def confirm_handler(message: types.Message):
-    chat_id = message.chat.id
-    user_id = message.from_user.id
-    
-    if chat_id not in confirmed_participants:
-        confirmed_participants[chat_id] = set()
-    
-    if user_id in confirmed_participants[chat_id]:
-        await message.answer(
-            f"❌ {message.from_user.first_name}, вы уже подтвердили участие!",
-            reply_markup=types.ReplyKeyboardRemove()
-        )
-        return
-    
-    confirmed_participants[chat_id].add(user_id)
-    await message.answer(
-        f"✅ {message.from_user.first_name}, ваше участие подтверждено!",
-        reply_markup=types.ReplyKeyboardRemove()
-    )
-
-@dispatcher.message(Command('finish'))
-async def finish_handler(message: types.Message):
-    chat_id = message.chat.id
-    
-    if chat_id not in confirmed_participants or not confirmed_participants[chat_id]:
-        await message.answer("❌ Никто не подтвердил участие!")
-        return
-    
-    participants = list(confirmed_participants[chat_id])
-    random.shuffle(participants)
-    
-    if chat_id not in group_members_map:
-        group_members_map[chat_id] = {}
-    
-    for index, user_id in enumerate(participants):
-        group_members_map[chat_id][user_id] = index + 1
-    
-    result_message = "🎲 Результаты:\n\n"
-    
-    for user_id, number in group_members_map[chat_id].items():
-        user = await odid_bot.get_chat_member(chat_id, user_id)
-        user_name = user.user.first_name
-        if user.user.last_name:
-            user_name += f" {user.user.last_name}"
-        result_message += f"{number}. {user_name}\n"
-    
-    await message.answer(result_message, reply_markup=throw_keyboard_button())
-    
-    confirmed_participants[chat_id] = set()
 
 async def run():
     """
@@ -96,5 +26,7 @@ async def run():
 
     await dispatcher.start_polling(
         odid_bot,
-        group_members_map=group_members_map
+        bot=odid_bot,
+        group_members_map={},
+        confirmed_participants={}
     )
--- routers/throwing.py
+++ routers/throwing.py
@@ -1,19 +1,90 @@
 import random
 
 import structlog
-from aiogram import Router, types, F
+from aiogram.filters import Command
+from aiogram import Router, types, F, Bot
+from aiogram.enums.chat_type import ChatType
 from aiogram.enums.parse_mode import ParseMode
 
 from constants import throw_actions
 from filters.chat_type import ChatTypeFilter
 from middlewares.admin_rules import AdminCheckMiddleware
 from middlewares.ensure_starting import EnsureStartedMiddleware
+from keyboards.for_throwing import throw_keyboard_button, confirm_participation_button
 
 logger = structlog.get_logger(__name__)
 router = Router()
-router.message.filter(ChatTypeFilter(chat_type=["group", "supergroup"]))
+router.message.filter(ChatTypeFilter(chat_type=[ChatType.GROUP, ChatType.SUPERGROUP]))
 router.message.middleware(AdminCheckMiddleware())
 router.message.middleware(EnsureStartedMiddleware())
+
+
+@router.message(Command('start'))
+async def start_handler(message: types.Message, confirmed_participants: dict):
+    chat_id = message.chat.id
+    if chat_id not in confirmed_participants:
+        confirmed_participants[chat_id] = set()
+    
+    await message.answer(
+        "👋 Привет! Подтверди участие, нажав на кнопку ниже.\n\nЕсли хотите завершить расчет, нажмите /finish",
+        reply_markup=confirm_participation_button()
+    )
+
+@router.message(lambda message: message.text == "Подтвердить участие")
+async def confirm_handler(message: types.Message, confirmed_participants: dict):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    
+    if chat_id not in confirmed_participants:
+        confirmed_participants[chat_id] = set()
+    
+    if user_id in confirmed_participants[chat_id]:
+        await message.answer(
+            f"❌ {message.from_user.first_name}, вы уже подтвердили участие!",
+            reply_markup=types.ReplyKeyboardRemove()
+        )
+        return
+    
+    confirmed_participants[chat_id].add(user_id)
+    await message.answer(
+        f"✅ {message.from_user.first_name}, ваше участие подтверждено!",
+        reply_markup=types.ReplyKeyboardRemove()
+    )
+
+@router.message(Command('finish'))
+async def finish_handler(
+    message: types.Message,
+    confirmed_participants: dict,
+    group_members_map: dict,
+    bot: Bot
+):
+    chat_id = message.chat.id
+    
+    if chat_id not in confirmed_participants or not confirmed_participants[chat_id]:
+        await message.answer("❌ Никто не подтвердил участие!")
+        return
+    
+    participants = list(confirmed_participants[chat_id])
+    random.shuffle(participants)
+    
+    if chat_id not in group_members_map:
+        group_members_map[chat_id] = {}
+    
+    for index, user_id in enumerate(participants):
+        group_members_map[chat_id][user_id] = index + 1
+    
+    result_message = "🎲 Результаты:\n\n"
+    
+    for user_id, number in group_members_map[chat_id].items():
+        user = await bot.get_chat_member(chat_id, user_id)
+        user_name = user.user.first_name
+        if user.user.last_name:
+            user_name += f" {user.user.last_name}"
+        result_message += f"{number}. {user_name}\n"
+    
+    await message.answer(result_message, reply_markup=throw_keyboard_button())
+    
+    confirmed_participants[chat_id] = set()
 
 
 @router.message(F.text.lower() == 'бросок')
