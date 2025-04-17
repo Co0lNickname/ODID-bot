8893a730e57e1064fcb63dd5174b84c34ac23734
DocLivsey <doclivsey@github.com>
2025-04-17 14:41:12 +0300

    feat: call dict from bot local storage

--- middlewares/ensure_starting.py
+++ middlewares/ensure_starting.py
@@ -4,16 +4,14 @@ from aiogram.dispatcher.middlewares.error import CancelHandler
 
 
 class EnsureStartedMiddleware(BaseMiddleware):
-    def __init__(self, user_numbers: dict):
-        self.user_numbers = user_numbers
-
     async def __call__(self, handler, event, data):
         if isinstance(event, types.Message):
+            users_dict = data.get('group_members_map', {})
             chat_id = event.chat.id
             user_id = event.from_user.id
             key = (chat_id, user_id)
 
-            if key not in self.user_numbers:
+            if key not in users_dict:
                 await event.answer("⚠️ Сначала введите /start, чтобы получить номер.")
                 raise CancelHandler()
 
