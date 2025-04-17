3514b8e38c65c72aea54ec866973c16006b6291f
DocLivsey <doclivsey@users.noreply.github.com>
2025-04-17 11:25:25 +0300

    feat: complete cache for members sequence numbers

--- bot/ODID_bot.py
+++ bot/ODID_bot.py
@@ -1,8 +1,12 @@
+import random
+
 import structlog
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from routers.throwing import router as throwing_router
+from keyboards.for_throwing import throw_keyboard_button
 from settings import telegram_bot_token
 
 logger = structlog.get_logger(__name__)
@@ -10,6 +14,19 @@ logger = structlog.get_logger(__name__)
 odid_bot = Bot(token=telegram_bot_token)
 storage = MemoryStorage()
 dispatcher = Dispatcher(storage=storage)
+
+
+@throwing_router.message(Command('start'))
+async def start_handler(message: types.Message, group_members_map: dict):
+    chat_id = message.chat.id
+    members = await odid_bot.get_chat_administrators(chat_id)
+    user_ids = [member.user.id for member in members]
+    random.shuffle(user_ids)
+    group_members_map[chat_id] = {user_id: index + 1 for index, user_id in enumerate(user_ids)}
+    await message.answer(
+        f"Список участников группы {group_members_map.items()} с рандомными порядковыми номерами.", 
+        reply_markup=throw_keyboard_button()
+    )
 
 
 async def run():
@@ -25,5 +42,6 @@ async def run():
     )
 
     await dispatcher.start_polling(
-        odid_bot
+        odid_bot,
+        group_members_map={}
     )
--- routers/throwing.py
+++ routers/throwing.py
@@ -2,23 +2,15 @@ import random
 
 import structlog
 from aiogram import Router, types, F
-from aiogram.filters import Command
 from aiogram.enums.parse_mode import ParseMode
-from aiogram.exceptions import TelegramBadRequest
 
-from constants import HELLO_TEXT, throw_actions
+from constants import throw_actions
 from filters.chat_type import ChatTypeFilter
 from middlewares.admin_rules import AdminCheckMiddleware
-from keyboards.for_throwing import throw_keyboard_button
 
 logger = structlog.get_logger(__name__)
 router = Router()
 router.message.filter(ChatTypeFilter(chat_type=["group", "supergroup"]))
-
-
-@router.message(Command('start'))
-async def start_handler(message: types.Message):
-    await message.reply(HELLO_TEXT, reply_markup=throw_keyboard_button())
 
 
 @router.message(F.text.lower() == 'бросок')
