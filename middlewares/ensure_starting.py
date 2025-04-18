a51a78e50df9ef4455a607c6b1739e4031409766
DocLivsey <doclivsey@users.noreply.github.com>
2025-04-18 20:53:19 +0300

    feat: change pass case

--- middlewares/ensure_starting.py
+++ middlewares/ensure_starting.py
@@ -10,11 +10,22 @@ class EnsureStartedMiddleware(BaseMiddleware):
             chat_id = event.chat.id
             user_id = event.from_user.id
             
-            if event.text in ['/start', 'Подтвердить участие', '/finish']:
+            text = event.text if event.text else ""
+            
+            if (
+                text == "Подтвердить участие" 
+                or text.startswith('/finish')
+                or text.startswith('/start') 
+            ):
                 return await handler(event, data)
                 
-            if chat_id not in group_members_map or user_id not in group_members_map.get(chat_id, {}):
-                await event.answer("⚠️ Сначала нужно подтвердить участие и дождаться окончания расчета (/finish).")
+            if (
+                chat_id not in group_members_map 
+                or user_id not in group_members_map.get(chat_id, {})
+            ):
+                await event.answer(
+                    "⚠️ Сначала нужно подтвердить участие и дождаться окончания расчета (/finish)."
+                )
                 raise CancelHandler()
 
         return await handler(event, data)
