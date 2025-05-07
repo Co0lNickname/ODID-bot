9b37893128d1220f3077cf0d8114b1de1a31286d
DocLivsey <doclivsey@github.com>
2025-05-07 21:13:05 +0300

    feat: error class about players count & get actions for count of players

--- constants.py
+++ constants.py
@@ -12,3 +12,21 @@ throw_actions = (
     'О/5', 'ДИ/5', 'Д/5',
     'О/6', 'ДИ/6', 'Д/6'
 )
+
+
+class InvalidPlayersCountException(Exception):
+    _msg = 'Ой-ой. Что-то не так с количеством игроков. Проверьте, что количество игроков от 2 до 6 человек'
+
+    def __init__(self):
+        super().__init__(self._msg)
+
+    @property
+    def msg(self):
+        return self._msg
+
+
+def get_throw_actions_for_players_count(players_count: int) -> tuple[str]:
+    if not (2 <= players_count <= 6):
+        raise InvalidPlayersCountException()
+
+    return throw_actions[:(3 * players_count)]
